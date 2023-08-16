ce39b53a85980ad4475e3ef2b288535429324702
dhritiman <100087905+dhritiman1@users.noreply.github.com>
2023-08-16 07:58:16 +0530

    made the carousel work

--- src/component/carousel.tsx
+++ src/component/carousel.tsx
@@ -1,5 +1,9 @@
 import type { Content } from "@/types";
 import Image from "next/image";
+import { useRef, useState } from "react";
+import type { ReactNode } from "react";
+
+type Direction = "left" | "right";
 
 type ItemProps = {
   title: string;
@@ -19,23 +23,80 @@ const Item = ({ title, path }: ItemProps) => {
   );
 };
 
+type ScrollButtonProps = {
+  dir: Direction;
+  className: string;
+  handleScroll: () => void;
+};
+
+const ScrollButton = ({ dir, className, handleScroll }: ScrollButtonProps) => {
+  return (
+    <button
+      className={
+        "absolute flex h-[140px] w-[3.125rem] items-center justify-center  opacity-100 transition-all duration-100 " +
+        className
+      }
+      onClick={handleScroll}
+    >
+      <div className="text-2xl font-bold">{dir === "left" ? "<" : ">"}</div>
+    </button>
+  );
+};
+
 type Props = {
   title: string;
   content: Content[];
 };
+
 export const Carousel = ({ title, content }: Props) => {
+  const [isLeftScrollable, setIsLeftScrollable] = useState(false);
+  const [isRightScrollable, setIsRightScrollable] = useState(true);
+  const carouselRef = useRef<HTMLDivElement>(null);
+
+  const scroll = (dir: Direction) => {
+    if (!carouselRef.current) return;
+
+    setIsLeftScrollable(true);
+
+    const { scrollLeft, clientWidth } = carouselRef.current;
+    const offset =
+      dir === "left" ? scrollLeft - clientWidth : scrollLeft + clientWidth;
+
+    carouselRef.current.scrollTo({ left: offset, behavior: "smooth" });
+  };
+
   return (
     <div className="mb-5">
       <div className="flex flex-col gap-2">
         <h2 className="text-xl">{title}</h2>
-        <div className="flex flex-[0_0_25%] gap-1 overflow-y-hidden scrollbar-none">
-          {content.map((item) => (
-            <Item
-              key={item.id}
-              title={item.title ?? item.name ?? ""}
-              path={item.backdrop_path ?? ""}
+        <div className="relative">
+          {isLeftScrollable && (
+            <ScrollButton
+              className="left-0 top-0 z-10 bg-gradient-to-r from-[#181818] to-[#18181875] hover:from-[#181818]"
+              dir="left"
+              handleScroll={() => scroll("left")}
             />
-          ))}
+          )}
+          {isRightScrollable && (
+            <ScrollButton
+              className="right-0 top-0 z-10 bg-gradient-to-l from-[#181818] to-[#18181875] hover:from-[#181818] hover:to-[#18181890]"
+              dir="right"
+              handleScroll={() => scroll("right")}
+            />
+          )}
+
+          <div
+            ref={carouselRef}
+            className="flex gap-1 overflow-y-hidden scrollbar-none"
+          >
+            {content.map((item) => (
+              <Item
+                key={item.id}
+                title={item.title ?? item.name ?? ""}
+                path={item.backdrop_path ?? ""}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </div>
